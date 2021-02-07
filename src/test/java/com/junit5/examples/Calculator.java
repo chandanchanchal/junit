93bd523b35f3213181b49638e4fbd3cb0115a1cc
chandanibmcloud <you@example.com>
2021-02-07 12:10:55 +0530

    d

---
+++ src/test/java/com/junit5/examples/Calculator.java
@@ -0,0 +1,12 @@
+package com.junit5.examples;
+
+public class Calculator {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public static int add(int i, int j) {
+		return i + j;
+	}
+
+}
