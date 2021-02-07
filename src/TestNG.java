17fd6ab977fa9ead371a1ecf95e04fcffb13d14d
chandanibmcloud <you@example.com>
2021-02-07 12:09:30 +0530

    ts

---
+++ src/TestNG.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNG {
+    @Test
+    public void f() {
+    }
+ 
+    @BeforeTest
+    public void beforeTest() {
+    }
+ 
+    @AfterTest
+    public void afterTest() {
+    }
+}
